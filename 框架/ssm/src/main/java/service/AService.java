ab58ee0aeb94e5bd5b7d6cb440a0b3f524f9b78f
qq904995993 <904995993@qq.com>
2018-08-02 16:12:07 +0800

    ssm + ehcache

--- 框架/ssm/src/main/java/service/AService.java
+++ 框架/ssm/src/main/java/service/AService.java
@@ -1,6 +1,7 @@
 package service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import dao.ADao;
@@ -12,6 +13,7 @@ public class AService{
 	@Autowired
 	private ADao aDao;
 	
+	@Cacheable(value="myCache", key="'getABy'+#id")  
 	public A getAById(String id) {
 		return aDao.getAById(id);
 	}
