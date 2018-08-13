129a73fe5d239d492f4fb6dae8afcff7501a51d9
qq904995993 <904995993@qq.com>
2018-08-13 15:57:49 +0800

    springcloud断路器

--- 框架/springcloud/EurekaConsumerFeign/src/main/java/com/example/demo/SchedualServiceHi.java
+++ 框架/springcloud/EurekaConsumerFeign/src/main/java/com/example/demo/SchedualServiceHi.java
@@ -4,8 +4,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("service-hi")
+@FeignClient(value = "service-hi", fallback = SchedualServiceHiHystric.class)
 public interface SchedualServiceHi {
-    @GetMapping("/hi")
+    
+	@GetMapping("/hi")
     String sayHiFromClientOne(@RequestParam("name") String name);
+	
 }
