4ffcaaf17ef2e005fcbfc408e02961d1ecdbf22d
qq904995993 <904995993@qq.com>
2018-08-22 17:25:46 +0800

    hystrix dashboard

--- framework/springcloud/eureka/ConsumerFeign/src/main/java/com/example/demo/EurekaConsumerFeignApplication.java
+++ framework/springcloud/eureka/ConsumerFeign/src/main/java/com/example/demo/EurekaConsumerFeignApplication.java
@@ -2,15 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+// @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class EurekaConsumerFeignApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaConsumerFeignApplication.class, args);
 	}
+	
 }
