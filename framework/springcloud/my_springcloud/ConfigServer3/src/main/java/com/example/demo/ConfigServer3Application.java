262782ad19a19c1e331f0c768e7f6df7777d7281
qq904995993 <904995993@qq.com>
2018-08-23 15:03:09 +0800

    my springcloud

---
+++ framework/springcloud/my_springcloud/ConfigServer3/src/main/java/com/example/demo/ConfigServer3Application.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigServer3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServer3Application.class, args);
+	}
+}
